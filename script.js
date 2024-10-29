604225c259309b67646a0021a7b1999b6609708b
SOUMEN MONDAL <151416831+Soumen-JavaDev@users.noreply.github.com>
2024-10-29 08:33:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 const makeChange = (c) => {
-  // your name here
+  const quarter=25;
+	const dime=10;
+	const nickel=5;
+	const penny=1;
+
+	const q=Math.floor(c/quarter);
+	c %=quarter;
+	const d=Math.floor(c/dime);
+	c %=dime;
+	const n=Math.floor(c/nickel);
+	c%-nickel;
+	const p=c;
+	return {q,d,n,p};
+	
 };
 
 // Do not the change the code below
